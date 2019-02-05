41790f7a79e50bbbedc2627694c39f2cab17e268
Shadowtyu <45743953+Shadowtyu@users.noreply.github.com>
2019-02-05 18:58:23 -0200

    Added Estalactites e arrendondamento da cave
    -Adicionado Estalactites
    -Arredondado plataformas da cave e parte nova
    (Arredondar a cave toda na proxima)

---
+++ Assets/Scripts/Mechanics/EstalactiteScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EstalactiteScript : MonoBehaviour {
+
+    Rigidbody rbody;
+    public GameObject player;
+    public AudioSource fallsound;
+
+    private void Start()
+    { 
+        rbody = GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        if (player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+        if (player.transform.position.y < -30)
+        {
+            if (Vector3.Distance(new Vector3(player.transform.position.x, 0, 0), new Vector3(this.transform.position.x, 0, 0)) < 9)
+            {
+                this.rbody.isKinematic = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            player.GetComponent<PlayerScript>().hitPoints -= 10;
+            fallsound.Play();
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnCollisionStay(Collision collider)
+    {
+        if (collider.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            fallsound.Play();
+            Destroy(this.gameObject);
+        }
+    }
+
+}
