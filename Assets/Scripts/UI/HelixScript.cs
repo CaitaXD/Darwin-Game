4c834f3d8b3d4b24098e44e2e8f1f288047008fe
Caetano <CaitaXD@Gmail.com>
2019-02-08 23:01:21 -0200

    More Tweaks in the menu again

--- Assets/Scripts/UI/HelixScript.cs
+++ Assets/Scripts/UI/HelixScript.cs
@@ -14,7 +14,8 @@ public class HelixScript : MonoBehaviour {
     [SerializeField]
     int selectionIs = 1;
     [SerializeField]
-    int menuHierachy =0;
+    int menuHierachy = 0;
+    int previusSelection;
 
 	// Use this for initialization
 	void Start () {
@@ -40,10 +41,12 @@ public class HelixScript : MonoBehaviour {
         switch (selectionIs)
         {
             case 1:
-                if (Input.GetKeyDown(KeyCode.Return))
+                if (Input.GetKeyDown(KeyCode.Return) && (menuHierachy == 0))
+                    
                 {
                     menuHierachy += 1;
                     anim.SetTrigger("fusions");
+                    previusSelection = 1;
                 }
                 slot1.sprite = Resume;
                 slot3.sprite = Fusions;
@@ -51,13 +54,18 @@ public class HelixScript : MonoBehaviour {
                 break;
 
             case 2:
-           
-                slot1.sprite = Exit;
+                if (Input.GetKeyDown(KeyCode.Return) && (menuHierachy == 0))
+                {
+                    Time.timeScale = 1;
+                    SceneManager.UnloadSceneAsync("menu");
+                    previusSelection = 2;
+                }
+                    slot1.sprite = Exit;
                 slot3.sprite = Resume;
                 slot2.sprite = Fusions;
                 break;
             case 3:
-                if (Input.GetKeyDown(KeyCode.Return))
+                if (Input.GetKeyDown(KeyCode.Return) && (menuHierachy == 0))
                 {
                     menuHierachy += 1;
                     Time.timeScale = 1;
@@ -71,10 +79,11 @@ public class HelixScript : MonoBehaviour {
                 break;
 
             case 4:
-                if (Input.GetKeyDown(KeyCode.Return))
+                if (Input.GetKeyDown(KeyCode.Return) && (menuHierachy == 0))
                 {
                     menuHierachy += 1;
                     anim.SetTrigger("options");
+                    previusSelection = 4;
                 }
 
                 slot1.sprite = Fusions;
@@ -86,13 +95,19 @@ public class HelixScript : MonoBehaviour {
                 if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.UpArrow))
         {
             selectionIs += 1;
-            anim.SetTrigger("HelixMoveright");
+            if (menuHierachy == 0)
+            {
+                anim.SetTrigger("HelixMoveright");
+            }
 
         }
         if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.DownArrow))
         {
             selectionIs -= 1;
-             anim.SetTrigger("HelixMoveLeft");
+            if (menuHierachy == 0)
+            {
+                anim.SetTrigger("HelixMoveLeft");
+            }
  
         }
         if (Input.GetKeyUp(KeyCode.Escape))
@@ -103,7 +118,11 @@ public class HelixScript : MonoBehaviour {
                 SceneManager.UnloadSceneAsync("menu");
                
             }
-            menuHierachy -= 1;
+            if (menuHierachy == 1)
+            {
+                selectionIs = previusSelection;
+            }
+                menuHierachy -= 1;
           
         }
     
